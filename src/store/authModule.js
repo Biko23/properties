1c0c710c3f106b48559a32145a939476a73720e2
Ssemugenyi Isaac <ssemugenyii@stanbic.com>
2021-08-21 11:39:23 +0300

    Handle login error

--- src/store/authModule.js
+++ src/store/authModule.js
@@ -46,9 +46,13 @@ const actions = {
     async login({ commit }, userDetails) {
         try {
             const response = await AuthService.loggingUser(userDetails);
-            if (response.status === 200 || response.status === 201) {
+            if (response.status === 200 && response.data.hasOwnProperty('token')) {
+                console.log(response.data.token);
                 localStorage.setItem('token', response.data.token);
                 commit('loginStatus', true);
+                return;
+            } else if (response.status === 200 && response.data.hasOwnProperty('status')) {
+                commit('loginStatus', false);
             }
             return response;
         } catch (error) {
